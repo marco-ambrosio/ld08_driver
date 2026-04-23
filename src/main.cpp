647cd7f3bfe607194136cb6a0c1911f59481ffff
Marco Ambrosio <62962632+marco-ambrosio@users.noreply.github.com>
2026-04-23 10:30:48 +0200

    Update src/main.cpp with new driver code

--- src/main.cpp
+++ src/main.cpp
@@ -14,80 +14,16 @@
 //
 // Author: LD Robot, Will Son
 
-#include <stdio.h>
-#include <iostream>
-
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/laser_scan.hpp>
-
-#include "../include/cmd_interface_linux.hpp"
-#include "../include/lipkg.hpp"
-#include "../include/transform.hpp"
-
+#include "../include/ld08_driver_component.hpp"
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("laser_scan_publisher");
-  rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr lidar_pub;
-  rclcpp::Rate loop_rate(100.0);
-
-  node->declare_parameter<std::string>("frame_id", "base_scan");
-  node->declare_parameter<std::string>("namespace", "");
-  std::string frame_id = node->get_parameter("frame_id").as_string();
-  std::string name_space = node->get_parameter("namespace").as_string();
-  if (name_space != "") {
-    frame_id = name_space + "/" + frame_id;
-  }
-
-  LiPkg * pkg;
-  std::string product;
-  int32_t ver = 8;
-  pkg = new LD08_LiPkg;
-
-  CmdInterfaceLinux cmd_port(ver);
-  std::vector<std::pair<std::string, std::string>> device_list;
-  std::string port_name;
-  cmd_port.GetCmdDevices(device_list);
-  for (auto n : device_list) {
-    std::cout << n.first << "    " << n.second << std::endl;
-    if (strstr(n.second.c_str(), "CP2102")) {
-      port_name = n.first;
-    }
-  }
-
-  if (port_name.empty() == false) {
-    std::cout << "FOUND LDS-02" << product << std::endl;
-    cmd_port.SetReadCallback(
-      [&pkg](const char * byte, size_t len) {
-        if (pkg->Parse((const uint8_t *)(byte), len)) {
-          pkg->AssemblePacket();
-        }
-      });
-
-    if (cmd_port.Open(port_name)) {
-      std::cout << "LDS-02" << product << " started successfully " << std::endl;
-    }
-
-    // char topic_name[20]={0};
-    // strcat(topic_name,product.c_str());
-    // strcat(topic_name,"/LDLiDAR");
-    lidar_pub = node->create_publisher<sensor_msgs::msg::LaserScan>(
-      "scan", rclcpp::QoS(rclcpp::SensorDataQoS())
-    );
-
-    while (rclcpp::ok()) {
-      if (pkg->IsFrameReady()) {
-        pkg->setStamp(node->now());
-        pkg->setFrameId(frame_id);
-        lidar_pub->publish(pkg->GetLaserScan());
-        pkg->ResetFrameReady();
-      }
-      loop_rate.sleep();
-    }
-  } else {
-    std::cout << "Can't find LDS-02" << product << std::endl;
-  }
-
+  rclcpp::NodeOptions options;
+  auto node = std::make_shared<ld08_driver::LD08DriverComponent>(options);
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
